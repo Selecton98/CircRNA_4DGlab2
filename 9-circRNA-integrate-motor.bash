0cb77b2e86ffb7ae882cc4d712505255985a6c3d
Selecton <51666666+Selecton98@users.noreply.github.com>
2021-06-14 11:57:03 +0800

    Update 9-circRNA-integrate-motor.bash

--- 9-circRNA-integrate-motor.bash
+++ 9-circRNA-integrate-motor.bash
@@ -1,23 +1,23 @@
 #!/bin/bash
 Rscript /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R --max-ppsize=500000
 
-sed -i "s/H1/H2/g"  /together_sdb/zhoujiaqi/ICCcircexosomewxz-circRNA-integrate.R
+sed -i "s/H1/H2/g"  /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R
 Rscript /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R --max-ppsize=500000
 
-sed -i "s/H2/H5/g"  /together_sdb/zhoujiaqi/ICCcircexosomewxz-circRNA-integrate.R
+sed -i "s/H2/H5/g"  /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R
 Rscript /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R --max-ppsize=500000
 
-sed -i "s/H5/H7/g"  /together_sdb/zhoujiaqi/ICCcircexosomewxz-circRNA-integrate.R
+sed -i "s/H5/H7/g"  /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R
 Rscript /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R --max-ppsize=500000
 
-sed -i "s/H7/Q3/g"  /together_sdb/zhoujiaqi/ICCcircexosomewxz-circRNA-integrate.R
+sed -i "s/H7/Q3/g"  /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R
 Rscript /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R --max-ppsize=500000
 
-sed -i "s/Q3/Q5/g"  /together_sdb/zhoujiaqi/ICCcircexosomewxz-circRNA-integrate.R
+sed -i "s/Q3/Q5/g"  /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R
 Rscript /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R --max-ppsize=500000
 
-sed -i "s/Q5/Q8/g"  /together_sdb/zhoujiaqi/ICCcircexosomewxz-circRNA-integrate.R
+sed -i "s/Q5/Q8/g"  /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R
 Rscript /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R --max-ppsize=500000
 
-sed -i "s/Q8/Q9/g"  /together_sdb/zhoujiaqi/ICCcircexosomewxz-circRNA-integrate.R
+sed -i "s/Q8/Q9/g"  /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R
 Rscript /together_sdb/zhoujiaqi/ICCcircexosome/wxz-circRNA-integrate.R --max-ppsize=500000
